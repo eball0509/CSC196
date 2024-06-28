5f94b469b8acb78b7e05c679a51c688f4490c247
eball0509 <161506233+eball0509@users.noreply.github.com>
2024-06-28 10:56:48 -0600

    initial commit

---
+++ Engine/Source/test.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+
+void print() {
+	cout << "yo!!!" << endl;
+};
---
+++ Game/Source/Main.cpp
@@ -0,0 +1,58 @@
+using namespace std;
+#include <iostream>
+#include "test.h"
+#include <SDL.h>
+#include <random>
+
+int main(int argc, char* argv[]) {
+	cout << "hello World" << endl;
+	print();
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	// create window
+	// returns pointer to window if successful or nullptr if failed
+	SDL_Window* window = SDL_CreateWindow("Game Engine",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		800, 600,
+		SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	// create renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	while (true)
+	{
+		// clear screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+
+		// draw line
+		int value = rand() % 1000;
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+		SDL_RenderDrawLine(renderer, 300, 300, 400, 200);
+
+		for (int i = 0; i < 100; i++)
+		{
+			SDL_SetRenderDrawColor(renderer, rand() % 255, rand() % 255, rand() % 255, 0);
+			SDL_RenderDrawLine(renderer, rand() % 800, rand() % 600, 800, 600);
+			SDL_RenderDrawPoint(renderer, rand() % 800, rand() % 600);
+
+		}
+		// show screen
+		SDL_RenderPresent(renderer);
+	}
+
+	return 0;
+
+}
